3871fc19746f108fb366f5baf1246d0fad6a924a
qlrtmxj <qlrtmxj@naver.com>
2026-08-10 11:25:50 +0900

    Q2056 ~ing

---
+++ src/SWEXPERT/jejeon322_star/D1/Q2056.java
@@ -0,0 +1,48 @@
+//연월일 달력
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class Q2056 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int cal = sc.nextInt();
+
+		/// int 변수를 [0:5],[5:7],[7:]으로 잘라서 
+		/// 각각을 int year, int month, int day로 저장하고싶음
+		
+		
+		
+		// 연월일조건에 안맞으면 -1 출력
+		 
+//		if(month <= 0 || month >12)
+//		 { if(month == 1, 3, 5, 7, 8, 10, 12 ) {
+//		 if(day != 31) {
+//		 		System.out.print(-1);
+//		 	} 
+//		 else if (month == 4,6,9,11) {
+//	 		if(day != 30) {
+//	 		System.out.print(-1);
+//		 		} 
+//		 	}
+//		 else if (month== 2) {
+//		 		if (day != 28) {
+//		 		System.out.print(-1);
+//				}
+//			}
+//		 
+//		 } System.out.print(-1);
+//		  
+//		 } System.out.print(-1);
+//		 
+//	  
+//		 //그리고 각각을 
+//		 System.out.printf("%d/%d/%d", year, month, day);
+		 
+		
+		
+		
+		
+		
+	}
