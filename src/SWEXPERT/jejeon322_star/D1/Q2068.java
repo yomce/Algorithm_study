600d354fd4280bbfb8eee0034928ed9b9b9f0e6d
qlrtmxj <qlrtmxj@naver.com>
2026-08-10 11:47:12 +0900

    Q2068 finish

---
+++ src/SWEXPERT/jejeon322_star/D1/Q2068.java
@@ -0,0 +1,30 @@
+package SWEXPERT.jejeon322_star.D1;
+
+import java.util.Scanner;
+
+public class Q2068 {
+	public static void main(String args[]) throws Exception{
+
+		 Scanner sc = new Scanner(System.in);
+	        int T;
+	        T=sc.nextInt();
+	        
+	        for(int test_case = 1; test_case <= T; test_case++)
+	        {
+	            int ans = 0 ;
+	    int[] arr = new int[10];
+	     
+	    for(int j = 0; j<10; j++) {
+	    arr[j] = sc.nextInt();
+	    if (ans <= arr[j]) {
+	        ans = arr[j];
+	        }
+	    }
+	 
+	    System.out.println("#"+test_case + " " + ans);
+	        }
+		
+		
+		
+	}
+}
