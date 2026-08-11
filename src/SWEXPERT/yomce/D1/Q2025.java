6cce075bc3a25d874981c6ef0969ec7bf8d58f64
kky032911 <kky032911@gmail.com>
2026-08-11 15:29:33 +0900

    solved Q2025

---
+++ src/SWEXPERT/yomce/D1/Q2025.java
@@ -0,0 +1,15 @@
+package SWEXPERT.yomce.D1;
+
+import java.util.Scanner;
+
+public class Q2025 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sum =0;
+		for (int i=1; i <= N; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
