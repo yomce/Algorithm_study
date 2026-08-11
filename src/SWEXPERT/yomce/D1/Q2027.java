5e19565c8734fca41aa5f1b77ada80b61b105d02
kky032911 <kky032911@gmail.com>
2026-08-11 15:23:20 +0900

    solved Q2027

---
+++ src/SWEXPERT/yomce/D1/Q2027.java
@@ -0,0 +1,11 @@
+package SWEXPERT.yomce.D1;
+
+public class Q2027 {
+	public static void main(String[] args) {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+	}
+}
