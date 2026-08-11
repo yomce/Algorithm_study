c2295bd813c5412044f153c3a20badbe0ce81cc0
kky032911 <kky032911@gmail.com>
2026-08-11 15:22:58 +0900

    solved Q2029

---
+++ src/SWEXPERT/yomce/D1/Q2029.java
@@ -0,0 +1,18 @@
+package SWEXPERT.yomce.D1;
+
+import java.util.Scanner;
+
+public class Q2029 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for (int i=0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+				
+			System.out.println("#"+(i+1)+" "+(a/b)+" "+(a%b));
+			
+		}
+	}
+}
