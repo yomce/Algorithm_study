feb03b833b396265a3ad3502d7b8a360d0df6fb4
abcdef512 <toy18809@gmail.com>
2026-08-13 08:46:48 +0900

    solve : 2046

---
+++ src/SWEXPERT/abcdef/D1/Q2046.java
@@ -0,0 +1,19 @@
+package SWEXPERT.abcdef.D1;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Q2046
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.print("#");
+
+		}
+	}
+}
--- src/SWEXPERT/abcdef/D1/Q2047.java
+++ src/SWEXPERT/abcdef/D1/Q2047.java
@@ -7,14 +7,18 @@ class Q2047
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
+		String T; 
+		T = sc.next();
+		String result ="";
+		for (int i = 0; i < T.length(); i ++) {
+			char a = T.charAt(i);
+			if (a >= 97) {
+				a -= 32;
+			} 
 			
-
+			result +=a;
 		}
+		System.out.println(result);
+
 	}
 }
