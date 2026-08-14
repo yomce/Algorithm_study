f2bb3997fab0e71d171b64192d10a40f7953052e
qlrtmxj <qlrtmxj@naver.com>
2026-08-14 11:19:17 +0900

    solve_Q2027

---
+++ src/SWEXPERT/jejeon322_star/D1/Q2027.java
@@ -0,0 +1,27 @@
+package SWEXPERT.jejeon322_star.D1;
+
+public class Q2027 {
+	public static void main(String[] args) {
+		
+		///	#++++
+		/// +#+++
+		/// ++#++
+		/// +++#+
+		/// ++++#
+
+		for(int i = 0 ; i<5; i++) {
+			for (int j = 0; j<i; j++) {
+				System.out.print("+");
+			}
+			System.out.print("#");
+			for (int j=0; j<5-1-i; j++) {
+				System.out.print("+");
+			}
+			System.out.println();
+		}
+		
+		
+
+
+	}
+}
