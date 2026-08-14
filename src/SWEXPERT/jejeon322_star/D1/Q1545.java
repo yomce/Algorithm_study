8eb3267bb47448857e864438ddcd53b545505d08
qlrtmxj <qlrtmxj@naver.com>
2026-08-14 13:04:37 +0900

    solve-Q1545

---
+++ src/SWEXPERT/jejeon322_star/D1/Q1545.java
@@ -0,0 +1,21 @@
+package SWEXPERT.jejeon322_star.D1;
+//거꾸로 출력해 보아요
+
+import java.util.Scanner;
+public class Q1545 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i = 0; i<=N ; i++) {
+			int j = N-i;
+			System.out.print(j + " ");
+			
+			
+		}
+		
+	}
+
+}
---
+++ src/SWEXPERT/jejeon322_star/D1/Q2019.java
@@ -0,0 +1,13 @@
+package SWEXPERT.jejeon322_star.D1;
+
+public class Q2019 {
+	public static void main(String[] args) {
+		//1부터 주어진 횟수까지 2를 곱한 값(들)을 출력
+		///
+		/// 
+	
+	
+	
+	}
+
+}
