a7a67f32587c6d8950977e2444641c2c2a534150
qlrtmxj <qlrtmxj@naver.com>
2026-08-14 11:17:03 +0900

    solve-Q2046

--- src/SWEXPERT/jejeon322_star/D1/Q2046.java
+++ src/SWEXPERT/jejeon322_star/D1/Q2046.java
@@ -1,17 +1,20 @@
 package SWEXPERT.jejeon322_star.D1;
-//스탬프 찍기
+//스탬프 찍기 
+
 import java.util.Scanner;
+
 public class Q2046 {
+	public static void main(String[] args) {
+		
+	
 	
 	Scanner sc = new Scanner(System.in);
-	int T = sc.nextInt();
-	String s = "#";
+	int T = sc.nextInt();	
 	
-	
-	
-	
-	for (int i = 0 ; i<T; i++) {
 		
+	for (int i = 0 ; i<T; i++) {
+		System.out.print("#");
 	}
 
+	}
 }
