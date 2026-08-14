29714e62f3c6f1820465ea5e459419a1dade0a2c
qlrtmxj <qlrtmxj@naver.com>
2026-08-14 11:16:19 +0900

    solve Q2047

--- src/SWEXPERT/jejeon322_star/D1/Q2047.java
+++ src/SWEXPERT/jejeon322_star/D1/Q2047.java
@@ -1,5 +1,16 @@
 package SWEXPERT.jejeon322_star.D1;
 // 신문 헤드라인
+
+import java.util.Scanner;
 public class Q2047 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String news = sc.next();
+		String ans = news.toUpperCase();
+		
+		System.out.println(ans);
+	} 
+	
 
 }
