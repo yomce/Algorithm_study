31afb338ebe302a4d772f0d9d7fcf7517b36c843
kky032911 <kky032911@gmail.com>
2026-08-19 17:58:20 +0900

    Q1288 -ing

---
+++ src/SWEXPERT/yomce/D2/Q1288.java
@@ -0,0 +1,16 @@
+package SWEXPERT.yomce.D2;
+
+import java.util.Scanner;
+
+public class Q1288 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			
+		}
+	}
+}
