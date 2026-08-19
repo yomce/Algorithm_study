850fbea030777bcb309563c41eaff8187fce94ec
김예진 <yejink0328@gmail.com>
2026-08-19 11:35:58 +0900

    solve: D1 1545 거꾸로 출력해 보아요

---
+++ src/SWEXPERT/yejink0328/D1/Q1545.java
@@ -0,0 +1,14 @@
+package D1;
+
+import java.util.Scanner;
+
+public class Q1545 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=N; i>=0; i--) {
+			System.out.print(i+" ");
+		}
+	}
+}
