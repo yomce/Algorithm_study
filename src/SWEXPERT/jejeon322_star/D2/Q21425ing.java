702af96744a1114b76b25b69b5a96ed26ecc567b
jje <je.jeon322@gmail.com>
2026-09-17 10:38:14 +0900

    Solve D2 Q21425

--- src/SWEXPERT/jejeon322_star/D2/Q21425ing.java
+++ src/SWEXPERT/jejeon322_star/D2/Q21425ing.java
@@ -1,80 +1,50 @@
 package SWEXPERT.jejeon322_star.D2;
+
 //+= ing
 import java.util.Scanner;
+
 public class Q21425ing {
 
-
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		
-		for(int test_case =1 ; test_case <= t ; test_case++) {
-			
-			
-			/// 각 줄에는 세 개의 정수 A B N이 공백 하나씩을 사이로 두고 주어진다.
-			/// x나 y 둘 중 하나 이상에 저장된 값이 N 초과가 되게 하기 위해 
-			/// “x += y”, “y += x” 연산을 최소 몇 번 수행해야 하는지 출력한다.
-			/// if(x + y > N ) { break }
-			/// for 돌려서 i출력?
+
+		for (int test_case = 1; test_case <= t; test_case++) {
+
+			/// x += y” 또는 “y += x” 연산을 원하는 순서대로 원하는 만큼 수행하여, 
+			/// x나 y 둘 중 하나 이상에 저장된 값이 N 초과가
+			/// 되게 하려고 한다. 연산을 합쳐서 최소 몇 번 수행해야 하는지 계산
 			/// 
-			/// 1 2 2 -> 1/ 1 2 3 -> 2 / 1 2 4 ->2 / 1 2 5 ->3 / 
-			/// 10 7 1293 ->11
-			/// x += y = 17      17 7
-			/// 
-			/// x += y = 24      24 7
-			/// y += x = 24		 17 24
-			/// 
-			/// if(x>y) {y += x ;} else if(y>x) {x += y;}
-			/// 
-			/// sum = x+y; 
-			/// if(x>y) {y = sum;} else if(y>x) {x = sum;}
-			/// 
-			
-			
+
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			int N = sc.nextInt();
 			
-			int sum = 0;
 			int ans = 0;
-			
-			
-			for(int i = 0; i< 1000; i++) {
-				
-					
-				if(x>y) {
-					y += x ;
-					sum = x + y;
-					break;
-				} else if(y>=x) {
-					x += y;
-					sum = x + y;
-					break;
-				} 
-				
-				
-				
-				if(x>y) {
-					y = sum;
-				} else if(y>=x) {
-					x = sum;
-				} 
+			int cnt= 0 ;
 
-				if(x + y > N ) { 
-				ans = i;
-				break; 
+			
+			while(true) {
+				if(x<y) {
+					x+=y;
+					cnt++;
+				} else {
+					y+=x;
+					cnt++;
 				}
+				if(x>N || y>N) break;
+				
 			}
 			
-			System.out.print(x + " ");
-			System.out.print(y + " ");
-			System.out.print(sum);
-			System.out.println();
-			System.out.println(ans);
-		}	
-		
-		
+			///10 7 1293
+			/// 0. 10 7 - > 1. 10 17 -> 2. 27 17 -> 3. 27 44 -> 4. 
+			/// 
 			
-	}	
+			
+			System.out.println(cnt);
+
+		}
+
+	}
 }
